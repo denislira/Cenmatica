8a1b1cc0eb67cb316ca067fca05273697aac4edc
denislira <denislira@live.com>
2019-03-25 09:55:35 -0300

    Enviando primeira vez o site

---
+++ index.php
@@ -0,0 +1,197 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <link rel="icon" href="imagens/favicon.ico">
+
+    <title>Cenmática 2019</title>
+
+    <!-- Bootstrap core CSS -->
+    <link href="bootstrap/css/bootstrap.min.css" rel="stylesheet">
+    <script src="bootstrap/jquery-3.3.1.min.js"></script>
+    <script src="bootstrap/js/bootstrap.min.js"></script>
+
+    <!-- Custom styles for this template -->
+    <link href="bootstrap/carousel.css" rel="stylesheet">
+    <style type="text/css">
+
+    </style>
+  </head>
+  <body>
+<a name="TOPO"></a><!--  ANCORA 1 -->
+    <header>
+      <nav class="navbar navbar-expand-lg navbar-dark fixed-top bg-dark">
+        <a class="navbar-brand" href="#"><img  src="imagens/logo.png" width="150px" ></a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarCollapse" aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarCollapse">
+          <ul class="navbar-nav mr-auto">
+            <li class="nav-item active">
+              <a class="nav-link" href="#TOPO">Início <span class="sr-only">(current)</span></a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#DOWNLOAD">Download</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#FORMATACAO">Formatação</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#SUPORTE">Advogados e Contadores</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#DICAS">Dicas</a>
+            </li>
+          </ul>
+          <form class="form-inline mt-2 mt-md-0" method="GET" action="">
+            <input class="form-control mr-sm-2" type="text" name="p" placeholder="Escreva.." aria-label="Search">
+            <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Pesquisar</button>
+          </form>
+        </div>
+      </nav>
+    </header>
+
+    <main role="main">
+
+  <div class="carousel slide" id="carousel" data-ride="carousel">
+    <ol class="carousel-indicators">
+      <li data-target="#carousel" data-slide-to="0" class="active" ></li>
+      <li data-target="#carousel" data-slide-to="1"></li>
+      <li data-target="#carousel" data-slide-to="2"></li>
+    </ol>
+    <div class="carousel-inner">
+      <div class="carousel-item active" style="height: 500px; background-color:#fff; ">
+        <!-- <img class="d-block w-100 h-auto" alt="Primeiro Slide" src="imagens/banner1.jpg"> -->
+        <div class="carousel-caption d-none d-lg-block" style="color:#000;">
+          <img  src="imagens/logo6.png" width="400px" >
+          <h1 class="display-2">Centro de Informática</h1>
+          <p class="lead">Instalação de Sistemas Operacionais, Recuperação de Arquivos, Remoção de Vírus</p>
+        </div>
+        <div class="carousel-caption d-block d-lg-none" style="color:#000;">
+           <img  src="imagens/logo6.png" width="70%" >
+          <h3 class="display-4">Centro de Informática</h3>
+          <p class="lead">Instalação de Sistemas Operacionais, Recuperação de Arquivos, Remoção de Vírus</p>
+        </div>
+      </div>
+
+ 
+    </div>
+    <a class="carousel-control-prev" role="button" href="#carousel" data-slide="prev">
+      <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+      <span class="sr-only">Anterior</span>
+    </a>
+    <a class="carousel-control-next" role="button" href="#carousel" data-slide="next">
+      <span class="carousel-control-next-icon" aria-hidden="true"></span>
+      <span class="sr-only">Próximo</span>
+    </a>
+  </div>
+
+      <!-- Marketing messaging and featurettes
+      ================================================== -->
+      <!-- Wrap the rest of the page in another container to center all the content. -->
+
+<a name="DOWNLOAD"></a><!--  ANCORA 2 -->
+
+      <div class="container-full marketing" >   
+  <?php  /*tamanho das imagens */ $tam = 170; ?>
+        <!-- Three columns of text below the carousel -->
+        <div class="row alert-secondary p-5 m-0">
+          <div class="col-lg-4">
+            <img class="rounded-circle" src="imagens/teamviewerQS10.png" alt="TeamViewer" width="<?=$tam?>" height="<?=$tam?>">
+            <h2>TeamViewerQS 10</h2>
+            <p>Versão Portátil, não necessita de instalação, Rápido e Prático.</p>
+            <p><a class="btn btn-secondary" href="https://download.teamviewer.com/download/version_10x/TeamViewerQS.exe" role="button">BAIXAR &raquo;</a></p>
+          </div><!-- /.col-lg-4 -->
+          <div class="col-lg-4">
+            <img class="rounded-circle" src="imagens/anydesk.png" alt="AnyDesk" width="<?=$tam?>" height="<?=$tam?>">
+            <h2>AnyDesk</h2>
+            <p>Segundo Programa mais usado pelos Técnicos, gratuito, menos veloz, mas Eficaz.</p>
+            <p><a class="btn btn-secondary" href="https://download.anydesk.com/AnyDesk.exe" role="button">BAIXAR &raquo;</a></p>
+          </div><!-- /.col-lg-4 -->
+          <div class="col-lg-4">
+            <img class="rounded-circle" src="imagens/teamviewer.png" alt="Generic placeholder image" width="<?=$tam?>" height="<?=$tam?>">
+            <h2>TeamViewer 13</h2>
+            <p>Versão COMPLETA, necessita ser INSTALADO.</p>
+            <p><a class="btn btn-secondary" href="https://download.teamviewer.com/download/version_13x/TeamViewer_Setup.exe" role="button">BAIXAR &raquo;</a></p>
+          </div><!-- /.col-lg-4 -->
+        </div><!-- /.row -->
+
+
+        <!-- START THE FEATURETTES -->
+
+<a name="FORMATACAO"></a><!--  ANCORA 3 -->
+
+        <hr class="featurette-divider m-0">
+
+        <div class="row featurette bg-danger p-5 m-0">
+          <div class="col-md-7 text-light">
+            <h2 class="featurette-heading">Sistemas Operacionais <span class="text-black-50"><p>Windows, Linux e Mac 
+              <h6><a href="" class="text-warning"> > Mais detalhes </a></h6></p></span></h2>
+            <p class="lead">Trabalhamos com essas plataformas e instalação dos programas essenciais para uso pessoal e empresarial.
+             <a href="" class="text-warning"> Veja Aqui</a> Todos os programas que instalamos.
+            </p>
+          </div>
+          <div class="col-md-5 p-0">
+            <img class="featurette-image img-fluid mx-auto" src="imagens/notebook.png" alt="image">
+          </div>
+        </div>
+
+        <hr class="featurette-divider  m-0 ">
+
+<a name="SUPORTE"></a><!--  ANCORA 4-->
+
+        <div class="row featurette bg-info m-0 pt-5 pl-3 pr-3">
+          <div class="col-md-7 order-md-2 text-light">
+            <h2 class="featurette-heading">Advogados e Contadores</h2><h3><span class="text-black-50"> Soluções e Correções para Sites e Programas</span></h3>
+            <p class="lead">Resolva problemas de acesso e configurações de Sites e Programas de seu serviço como:
+            Instalação de Certificados, Configuração de JAVA e liberação de Segurança, Configuração de 
+            TRT, PJE, CAIXA, CONECTIVIDADE SOCIAL, CAJED, SEFIP, E-CAC, HOMOLOGNET, Emissor de Notas Fiscais, Internetbanking, e muito mais.</p>
+          </div>
+          <div class="col-md-5 order-md-1 p-0">
+            <img class="featurette-image img-fluid mx-auto" src="imagens/pessoas.png" alt="Generic placeholder image">
+          </div>
+        </div>
+
+        <hr class="featurette-divider m-0">
+
+<a name="DICAS"></a><!--  ANCORA 5-->
+
+        <div class="row featurette bg-warning p-5 m-0">
+          <div class="col-md-7">
+            <h2 class="featurette-heading">Aprenda Mais, <span class="text-black-50"> Veja Dicas</span></h2>
+            <p class="lead">Aprenda questões Básicas que todo mundo deveria saber, sobre Segurança, Antivírus, 
+            Sistema Operacional, e dicas em Gerais.</p>
+          </div>
+          <div class="col-md-5">
+            <img class="featurette-image img-fluid mx-auto" src="imagens/dicas.png" alt="Generic placeholder image">
+          </div>
+        </div>
+
+        <hr class="featurette-divider mt-0">
+
+        <!-- /END THE FEATURETTES -->
+
+      </div><!-- /.container -->
+
+
+      <!-- FOOTER -->
+      <footer class="container">
+        <p class="float-right"><a href="#">Voltar ao Topo</a></p>
+        <p>&copy; 2017-2018 Company, Inc. &middot; <a href="#">Privacy</a> &middot; <a href="#">Terms</a></p>
+      </footer>
+    </main>
+
+    <!-- Bootstrap core JavaScript
+    ================================================== -->
+    <!-- Placed at the end of the document so the pages load faster -->
+    <!-- <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script> -->
+    <!-- <script>window.jQuery || document.write('<script src="bootstrap/jquery-slim.min.js"><\/script>')</script> -->
+    <!-- <script src="bootstrap/popper.min.js"></script> -->
+    <!-- <script src="bootstrap/css/bootstrap.min.js"></script> -->
+    <!-- Just to make our placeholder images work. Don't actually copy the next line! -->
+    <!-- <script src="bootstrap/holder.min.js"></script> -->
+  </body>
+</html>
